7fcc9ee0409aedce8beabcf14aae3ee4531c5fc6
Vadim Gorai <vadimgorai@gmail.com>
2020-09-10 20:18:53 +0300

    updated responsive layout

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
+
 $(document).ready(function() {
-    $('.nav-burger').click(function(e) {
+    $('.nav-burger').click( () => {
         $('.nav-burger, .nav-layout').toggleClass('active');
     });
 
-    $('.nav-layout a').click(function(e) {
+    $('.nav-layout a').click( () => {
         $('.nav-layout, .nav-burger').removeClass('active');
     });
 });
